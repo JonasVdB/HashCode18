ebec839c2bb180c095f3ec18f04250aa17ec8878
Unknown <robin.depauw5@gmail.com>
2018-03-01 19:34:44 +0100

    command center basics

--- src/CommandCenter.py
+++ src/CommandCenter.py
@@ -1,7 +1,9 @@
+from src.Location import Location
 class CommandCenter:
     def __init__(self):
         self.rides = list()
         self.cars = list()
+        self.simTime = 0
 
     def planAllCars(self):
         for car in self.cars:
@@ -10,3 +12,25 @@ class CommandCenter:
     def planCar(self, car):
         time = 0
         location = Location(0,0)
+        while time<=self.simTime:
+            #Select a ride to add to the car
+            selected_ride = self.findClosestRide(location,time,self.rides,self.simTime)
+            if selected_ride is None:
+                break
+            #add the ride to the car
+            car.addNewRide(selected_ride)
+            self.rides.remove(selected_ride)
+
+            #adjust the simulation time
+            time += self.travelTime(location,selected_ride.start_location)
+            if time<selected_ride.start_time:
+                time = selected_ride.start_time
+            time += self.travelTime(selected_ride.start_location, selected_ride.finish_location)
+
+
+    @staticmethod
+    def travelTime(loc1, loc2):
+        return
+
+    def findClosestRide(self,carLocation,currentTime,rides,maxSimTime):
+        return
