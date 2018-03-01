c9af6684c6205e6634ed5a5d161a3086891eba34
Jonas Vanden Branden <vandenbranden.jonas@gmail.com>
2018-03-01 19:19:12 +0100

    Added distance and ignore pyc files

--- src/Location.py
+++ src/Location.py
@@ -3,6 +3,12 @@ class Location:
         self.col = col
         self.row = row
 
+    def distance(self, otherlocation):
+        x = self.col - otherlocation.col
+        y = self.row - otherlocation.row
+        dist = abs(x) + abs(y)
+        return dist
+
     def __str__(self):
         return "<"+str(self.row) + "," + str(self.col) + ">"
 
